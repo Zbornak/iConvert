8598f3dfe60c58fc9e6e03a43c181f8116760586
zbornak <zbornak@protonmail.com>
2024-01-10 10:54:18 +0000

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Mark Strijdom on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
