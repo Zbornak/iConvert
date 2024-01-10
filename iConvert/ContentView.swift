e48987c7b62184c1b0c9c1c34bbd1ac6adce6dba
zbornak <zbornak@protonmail.com>
2024-01-10 11:34:16 +0000

    Created initial UI

--- iConvert/ContentView.swift
+++ iConvert/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput = 0.0
+    @State private var selectedConversionFromOption = "seconds"
+    @State private var selectedConversionToOption = "seconds"
+    @State private var result = 0.0
+    
+    let conversionOptions = ["seconds", "minutes", "hours", "days"]
+    
+    @FocusState private var numberIsFocused: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Number to convert", value: $userInput, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($numberIsFocused)
+                } header: {
+                    Text("number to convert")
+                }
+                
+                Section {
+                    Picker("Convert from", selection: $selectedConversionFromOption) {
+                        ForEach(conversionOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("convert from")
+                }
+                
+                Section {
+                    Picker("Convert to", selection: $selectedConversionToOption) {
+                        ForEach(conversionOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("convert to")
+                }
+                
+                Section {
+                    Text("\(userInput) \(selectedConversionFromOption) is \(result) \(selectedConversionToOption).")
+                } header: {
+                    Text("result")
+                }
+            }
+            .navigationTitle("iConvert")
+            .toolbar {
+                if numberIsFocused {
+                    Button("Done") {
+                        numberIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
